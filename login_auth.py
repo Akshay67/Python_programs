68f1d3820da56ca1c9e4917ffb6882d77b16769e
Akshay67 <akshayshivekar06081995@gmail.com>
2020-03-03 23:55:06 +0530

    Login auth task is completed

--- login_auth.py
+++ login_auth.py
@@ -1,17 +1,7 @@
-cmdUsername = input("Enter the username: ")
-cmdPassword = input("Enter the password: ")
-
+'''
 def storeUserDetails(username,password):
     with open('userdeatil.txt','w') as f:
         f.write("username- {}\npassword- {}".format(username,password))            
-'''
-def getUserDetails():
-    with open('userdeatil.txt','r') as f:
-        getUserDetails.uname = (f.readline().split('- ')[1])
-        getUserDetails.passwd = (f.readline().split('- ')[1])
-    return (getUserDetails.uname,getUserDetails.passwd)
-    #print(getUserDetails.uname)
-    #print(getUserDetails.passwd)
 '''
 def loginUser(username,password):
     with open('userdeatil.txt','r') as f:
@@ -23,7 +13,36 @@ def loginUser(username,password):
     else:
         print('login failed..!')
 
+def userRegistration(username,password):
+    with open('userdeatil.txt','w') as f:
+        f.write("username- {}\npassword- {}".format(username,password))
+    print("Congratulation Your account has been created successfully.")            
+
+choice = input("Do you have already account to login: Y/N -> ")
+
+if choice =='Y' or 'y':
+    cmdUsername = input("Enter the username: ")
+    cmdPassword = input("Enter the password: ")
+    loginUser(cmdUsername,cmdPassword)
+
+elif choice == 'N' or 'n':
+    cmdUsername = input("Please Enter the username to create account: ")
+    cmdPassword = input("PLease Enter the password to create account: ")
+    userRegistration(cmdUsername,cmdPassword)
+else:
+    print("Something went wrong.!")
+
 
 #storeUserDetails(cmdUsername,cmdPassword)
 #getUserDetails()
-loginUser(cmdUsername,cmdPassword)
+#nloginUser(cmdUsername,cmdPassword)
+
+'''
+def getUserDetails():
+    with open('userdeatil.txt','r') as f:
+        getUserDetails.uname = (f.readline().split('- ')[1])
+        getUserDetails.passwd = (f.readline().split('- ')[1])
+    return (getUserDetails.uname,getUserDetails.passwd)
+    #print(getUserDetails.uname)
+    #print(getUserDetails.passwd)
+'''
